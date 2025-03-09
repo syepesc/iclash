d86efd1cbf27d8fe343a684421f500f52dd4727c
Santiago Yepes <santiagoyepescarrera@gmail.com>
2025-03-09 03:06:46 -0400

    Initial clash api behaviour progress

---
+++ lib/iclash/clash_api/client.ex
@@ -0,0 +1,60 @@
+defmodule Iclash.ClashApi.Client do
+  @moduledoc """
+  Clash of Clans API Behaviour
+  """
+
+  # The otp_app config key is used to lookup the implementation dynamically.
+  # By default the lookup happens during runtime for the TEST env build and at
+  # compile time for all other builds
+  use Knigge,
+    otp_app: :iclash,
+    default: Iclash.ClashApi.Client.ClientImpl
+
+  alias Iclash.ClashApi.Models.ClientError
+  alias Iclash.ClashApi.Models.PlayerTag
+  alias Iclash.ClashApi.Models.Player
+
+  @callback get_player(player_tag :: PlayerTag.t()) ::
+              {:ok, Player.t()} | {:error, ClientError.t()} | {:error, any()}
+end
+
+defmodule Iclash.ClashApi.Client.ClientImpl do
+  @moduledoc """
+  Clash of Clans API Implementation
+  """
+  @behaviour Iclash.ClashApi.Client
+
+  alias Iclash.ClashApi.Models.ClientError
+  alias Iclash.ClashApi.Models.PlayerTag
+  alias Iclash.ClashApi.Models.Player
+
+  require Logger
+
+  def get_player(%PlayerTag{tag: tag}) do
+    response = Req.get(base_request(), url: "/players/#{tag}")
+
+    case response do
+      {:ok, %Req.Response{status: 200} = response} ->
+        {:ok, Player.new(response.body)}
+
+      {:ok, %Req.Response{status: _} = response} ->
+        Logger.error("Clash API error. response=#{inspect(response)}")
+        {:error, ClientError.new(response.body)}
+
+      {:error, reason} ->
+        Logger.error("Req library error. reason=#{inspect(reason)}")
+        {:error, reason}
+    end
+  end
+
+  defp api_token, do: Application.fetch_env!(:iclash, ClashApiConfig)[:api_token]
+  defp base_url, do: Application.fetch_env!(:iclash, ClashApiConfig)[:base_url]
+
+  defp base_request() do
+    Req.new(
+      retry: :transient,
+      auth: {:bearer, api_token()},
+      base_url: base_url()
+    )
+  end
+end
