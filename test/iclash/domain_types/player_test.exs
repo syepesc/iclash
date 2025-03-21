d39c6dcbc7c9d5fb96310ca6408627f416bbfc06
Santiago Yepes <santiagoyepescarrera@gmail.com>
2025-03-21 10:15:22 -0400

    Add tests for Player domain type

--- test/iclash/domain_types/player_test.exs
+++ test/iclash/domain_types/player_test.exs
@@ -9,123 +9,166 @@ defmodule Iclash.DomainTypes.PlayerTest do
 
     heroes_map = [
       %{
-        # "player_id" => "#P1",
-        "name" => "HERO 1",
-        "level" => 100,
-        "max_level" => 100,
-        "village" => "home",
-        "inserted_at" => now,
-        "updated_at" => now
+        player_tag: "#P1",
+        name: "HERO 1",
+        level: 50,
+        max_level: 100,
+        village: "home",
+        inserted_at: now,
+        updated_at: now
+      },
+      %{
+        player_tag: "#P1",
+        name: "HERO 2",
+        level: 80,
+        max_level: 100,
+        village: "home",
+        inserted_at: now,
+        updated_at: now
       }
     ]
 
-    player_map =
+    {:ok, player} =
       %{
-        "tag" => "#P1",
-        "name" => "PLAYER 1",
-        "trophies" => 100,
-        "town_hall_level" => 17,
-        "best_trophies" => 100,
-        "attack_wins" => 10,
-        "defense_wins" => 10,
-        "role" => "admin",
-        "war_preference" => "in",
-        "heroes" => heroes_map,
-        "inserted_at" => now,
-        "updated_at" => now
+        tag: "#P1",
+        name: "PLAYER 1",
+        trophies: 100,
+        town_hall_level: 17,
+        best_trophies: 100,
+        attack_wins: 10,
+        defense_wins: 10,
+        role: "admin",
+        war_preference: "in",
+        heroes: heroes_map,
+        inserted_at: now,
+        updated_at: now
       }
+      |> PlayerSchema.to_struct()
 
-    %{player: player_map}
+    %{now: now, player: player}
   end
 
   describe "get_player/1" do
-    test "Gets a player from database return player struct", %{player: player} do
-      %PlayerSchema{}
-      |> PlayerSchema.changeset(player)
-      |> IO.inspect(label: "CS -->")
-      |> Repo.insert()
-
+    test "Gets a player and return player struct", %{player: player} do
       {:ok, inserted_player} = Player.upsert_player(player)
-      player_form_db = Player.get_player(player["tag"])
-      assert player_form_db == inserted_player
+      player_form_db = Player.get_player(player.tag)
+      assert inserted_player == player_form_db
     end
 
-    test "Gets a player from databse return error tuple if not found", %{player: player} do
-      result = Player.get_player(player["tag"])
+    test "Gets a player and return error tuple if not found", %{player: player} do
+      result = Player.get_player(player.tag)
       assert result == {:error, :not_found}
     end
   end
 
-  # describe "upsert_player/1" do
-  #   test "Creates a player", %{player: player} do
-  #     {:ok, player_form_db} = Player.upsert_player(player)
-  #     assert player_form_db == Player.get_player(player.tag)
-  #   end
+  describe "upsert_player/1" do
+    test "Creates a player", %{player: player} do
+      {:ok, player_form_db} = Player.upsert_player(player)
+      assert player_form_db == Player.get_player(player.tag)
+    end
 
-  #   test "Updates a player", %{player: player} do
-  #     new_name = "NEW NAME"
-  #     updated_player = Map.put(player, :name, new_name)
+    test "Updates a player", %{player: player} do
+      new_name = "NEW NAME"
+      updated_player = Map.put(player, :name, new_name)
 
-  #     {:ok, player_from_db} = Player.upsert_player(player)
-  #     {:ok, updated_player_from_db} = Player.upsert_player(updated_player)
+      {:ok, _player_from_db} = Player.upsert_player(player)
+      {:ok, updated_player_from_db} = Player.upsert_player(updated_player)
 
-  #     assert updated_player_from_db.name == new_name
-  #     assert Map.drop(player_from_db, [:name]) == Map.drop(updated_player_from_db, [:name])
-  #   end
+      assert updated_player_from_db.name == new_name
+    end
 
-  #   test "Updates a player,  change updated_at and keeps inserted_at", %{player: player} do
-  #     new_name = "NEW NAME"
-  #     # Instead of keeping the fixed timestamps from the player passed through the setup function,
-  #     # the timestamps should be set to nil to let Ecto handle them internally.
-  #     updated_player =
-  #       player
-  #       |> Map.put(:name, new_name)
-  #       |> Map.put(:updated_at, nil)
-  #       |> Map.put(:insterted_at, nil)
+    test "Updates a player, change updated_at and keeps inserted_at", %{player: player} do
+      new_name = "NEW NAME"
+      # Instead of keeping the fixed timestamps from the player passed through the setup function,
+      # the timestamps should be set to nil to let Ecto handle them internally.
+      updated_player =
+        player
+        |> Map.put(:name, new_name)
+        |> Map.put(:updated_at, nil)
+        |> Map.put(:insterted_at, nil)
 
-  #     {:ok, player_from_db} = Player.upsert_player(player)
-  #     {:ok, updated_player_from_db} = Player.upsert_player(updated_player)
+      {:ok, player_from_db} = Player.upsert_player(player)
+      {:ok, updated_player_from_db} = Player.upsert_player(updated_player)
 
-  #     assert player_from_db.updated_at != updated_player_from_db.updated_at
-  #     assert player_from_db.inserted_at == updated_player_from_db.inserted_at
-  #   end
+      assert player_from_db.updated_at != updated_player_from_db.updated_at
+      assert player_from_db.inserted_at == updated_player_from_db.inserted_at
+    end
 
-  #   # test "Updates a player heroes association", %{player: player} do
-  #   #   new_hero_name = "NEW NAME"
-  #   #   updated_heroe = Map.put(hd(player.heroes), :name, new_hero_name)
-  #   #   updated_player = Map.put(player, :heroes, [updated_heroe])
+    test "Updates a player heroes association", %{player: player} do
+      new_hero_name = "NEW NAME"
+      updated_heroe = Map.put(hd(player.heroes), :name, new_hero_name)
+      updated_player = Map.put(player, :heroes, [updated_heroe])
 
-  #   #   {:ok, player_from_db} = Player.upsert_player(player)
-  #   #   {:ok, updated_player_from_db} = Player.upsert_player(updated_player)
+      {:ok, player_from_db} = Player.upsert_player(player)
+      {:ok, updated_player_from_db} = Player.upsert_player(updated_player)
 
-  #   #   assert hd(updated_player_from_db.heroes).name == new_hero_name
+      assert hd(updated_player_from_db.heroes).name == new_hero_name
 
-  #   #   assert Map.drop(hd(player_from_db.heroes), [:name]) ==
-  #   #            Map.drop(hd(updated_player_from_db.heroes), [:name])
-  #   # end
+      assert Map.drop(hd(player_from_db.heroes), [:name]) ==
+               Map.drop(hd(updated_player_from_db.heroes), [:name])
+    end
 
-  #   # test "Updating a player heroes association change updated_at and keeps inserted_at", %{
-  #   #   player: player
-  #   # } do
-  #   #   new_hero_name = "NEW NAME"
-  #   #   # Instead of keeping the fixed timestamps from the player passed through the setup function,
-  #   #   # the timestamps should be set to nil to let Ecto handle them internally.
-  #   #   updated_heroe =
-  #   #     player.heroes
-  #   #     |> hd()
-  #   #     |> Map.put(:name, new_hero_name)
-  #   #     |> Map.put(:updated_at, nil)
-  #   #     |> Map.put(:insterted_at, nil)
+    test "Updating a player heroes association change updated_at and keeps inserted_at", %{
+      player: player
+    } do
+      new_hero_name = "NEW NAME"
+      # Instead of keeping the fixed timestamps from the player passed through the setup function,
+      # the timestamps should be set to nil to let Ecto handle them internally.
+      updated_heroe =
+        player.heroes
+        |> hd()
+        |> Map.put(:name, new_hero_name)
+        |> Map.put(:updated_at, nil)
+        |> Map.put(:insterted_at, nil)
 
-  #   #   updated_player = Map.put(player, :heroes, [updated_heroe])
+      updated_player = Map.put(player, :heroes, [updated_heroe])
 
-  #   #   {:ok, player_from_db} = Player.upsert_player(player)
-  #   #   {:ok, updated_player_from_db} = Player.upsert_player(updated_player)
+      {:ok, player_from_db} = Player.upsert_player(player)
+      {:ok, updated_player_from_db} = Player.upsert_player(updated_player)
 
-  #   #   assert hd(player_from_db.heroes).updated_at != hd(updated_player_from_db.heroes).updated_at
+      assert hd(player_from_db.heroes).updated_at != hd(updated_player_from_db.heroes).updated_at
 
-  #   #   assert hd(player_from_db.heroes).inserted_at ==
-  #   #            hd(updated_player_from_db.heroes).inserted_at
-  #   # end
-  # end
+      assert hd(player_from_db.heroes).inserted_at ==
+               hd(updated_player_from_db.heroes).inserted_at
+    end
+
+    test "Keeps track of changes in player heroes", %{now: now, player: player} do
+      new_heroes = [
+        # New hero, now database shuold have 3 heroes.
+        %{
+          player_tag: "#P1",
+          name: "HERO 3",
+          level: 100,
+          max_level: 100,
+          village: "home",
+          inserted_at: DateTime.add(now, 8, :minute),
+          updated_at: DateTime.add(now, 8, :minute)
+        },
+        # Update existing hero level, now database shuold have 4 heroes counting the previous record for this hero.
+        %{
+          player_tag: "#P1",
+          name: "HERO 2",
+          level: 100,
+          max_level: 100,
+          village: "home",
+          inserted_at: DateTime.add(now, 8, :minute),
+          updated_at: DateTime.add(now, 8, :minute)
+        }
+      ]
+
+      {:ok, new_player} =
+        player
+        |> PlayerSchema.to_map()
+        |> Map.put(:heroes, new_heroes)
+        |> PlayerSchema.to_struct()
+
+      {:ok, player_from_db} = Player.upsert_player(player)
+      {:ok, updated_player_from_db} = Player.upsert_player(new_player)
+
+      # There should be 2 heroes after inserting the player.
+      assert length(player_from_db.heroes) == 2
+      # There should be 4 after updating. 2 existing heroes, 1 new hero and 1 updated hero.
+      assert length(updated_player_from_db.heroes) == 4
+    end
+  end
 end
