12359d4be3e06d423a19de7212242b795c461965
Santiago Yepes <santiagoyepescarrera@gmail.com>
2025-03-22 12:41:16 -0400

    Add migrations for troops and spells

---
+++ priv/repo/migrations/20250322163905_add_troops_table.exs
@@ -0,0 +1,23 @@
+defmodule Iclash.Repo.Migrations.AddTroopsTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:troops, primary_key: false) do
+      add :player_tag,
+          references(:players,
+            column: :tag,
+            type: :string,
+            on_delete: :delete_all,
+            on_update: :update_all
+          ),
+          primary_key: true
+
+      add :name, :string, primary_key: true
+      add :level, :integer, primary_key: true
+      add :max_level, :integer, null: false
+      add :village, :string, null: false
+
+      timestamps(type: :utc_datetime_usec)
+    end
+  end
+end
