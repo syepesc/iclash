074fd0ad9632cff1e626d6d9c2536e643e98e448
Santiago Yepes <santiagoyepescarrera@gmail.com>
2025-03-29 23:57:20 -0400

    Remove inner unused module

--- priv/repo/migrations/20250315162357_add_players_table.exs
+++ priv/repo/migrations/20250315162357_add_players_table.exs
@@ -1,27 +1,23 @@
 defmodule Iclash.Repo.Migrations.AddPlayersTable do
   use Ecto.Migration
 
-  defmodule Iclash.Repo.Migrations.CreatePlayers do
-    use Ecto.Migration
+  def change do
+    create table(:players, primary_key: false) do
+      add :tag, :string, primary_key: true
+      add :name, :string, null: false
+      add :trophies, :integer, null: false
+      add :town_hall_level, :integer, null: false
+      add :best_trophies, :integer, null: false
+      add :war_stars, :integer, null: false
+      add :attack_wins, :integer, null: false
+      add :defense_wins, :integer, null: false
+      add :exp_level, :integer, null: false
+      add :role, :string, null: false
+      add :war_preference, :string, null: false
 
-    def change do
-      create table(:players, primary_key: false) do
-        add :tag, :string, primary_key: true
-        add :name, :string, null: false
-        add :trophies, :integer, null: false
-        add :town_hall_level, :integer, null: false
-        add :best_trophies, :integer, null: false
-        add :war_stars, :integer, null: false
-        add :attack_wins, :integer, null: false
-        add :defense_wins, :integer, null: false
-        add :exp_level, :integer, null: false
-        add :role, :string, null: false
-        add :war_preference, :string, null: false
-
-        timestamps(type: :utc_datetime_usec)
-      end
-
-      create unique_index(:players, [:tag])
+      timestamps(type: :utc_datetime_usec)
     end
+
+    create unique_index(:players, [:tag])
   end
 end
