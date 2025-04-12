2daa2c21610ecb80a0642943d377ff531ae1e4ec
Santiago Yepes <santiagoyepescarrera@gmail.com>
2025-04-12 15:17:06 -0400

    Add observer cli

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Iclash.MixProject do
   def application do
     [
       mod: {Iclash.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :wx, :observer]
     ]
   end
 
