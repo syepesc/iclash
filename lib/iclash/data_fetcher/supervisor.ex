b132f85a5d658f175b22e2d28d9512e28065e08f
Santiago Yepes <santiagoyepescarrera@gmail.com>
2025-06-01 08:26:00 -0400

    Create database stats fetcher

--- lib/iclash/data_fetcher/supervisor.ex
+++ lib/iclash/data_fetcher/supervisor.ex
@@ -1,6 +1,8 @@
 defmodule Iclash.DataFetcher.Supervisor do
   @moduledoc false
 
+  # TODO: Add start/stop functions to manually control this supervisor
+
   use Supervisor
 
   # This defines the Clash API rate limit: 40 request per second, this number is based on experimentation because their API does not provide one.
@@ -15,9 +17,9 @@ defmodule Iclash.DataFetcher.Supervisor do
   def init(:ok) do
     children = [
       {Registry, name: Iclash.Registry.DataFetcher, keys: :unique},
-      {DynamicSupervisor,
-       name: Iclash.DataFetcher, strategy: :one_for_one, max_children: @rate_limit},
-      {Iclash.DataFetcher.Queue, %{rate_limit: @rate_limit, rate_limit_ms: @rate_limit_ms}}
+      {DynamicSupervisor, name: Iclash.DataFetcher, strategy: :one_for_one},
+      {Iclash.DataFetcher.Queue, %{rate_limit: @rate_limit, rate_limit_ms: @rate_limit_ms}},
+      {Iclash.DataFetcher.DbStatsFetcher, []}
     ]
 
     Supervisor.init(children, strategy: :one_for_one)
