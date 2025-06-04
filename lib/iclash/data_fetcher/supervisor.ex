2f5987ee8f53ccb54ce7c81eefd5e6e3c7246d8e
Santiago Yepes <santiagoyepescarrera@gmail.com>
2025-06-03 21:25:33 -0500

    Revert "Create database stats fetcher"
    This reverts commit b132f85a5d658f175b22e2d28d9512e28065e08f.

--- lib/iclash/data_fetcher/supervisor.ex
+++ lib/iclash/data_fetcher/supervisor.ex
@@ -1,8 +1,6 @@
 defmodule Iclash.DataFetcher.Supervisor do
   @moduledoc false
 
-  # TODO: Add start/stop functions to manually control this supervisor
-
   use Supervisor
 
   # This defines the Clash API rate limit: 40 request per second, this number is based on experimentation because their API does not provide one.
@@ -38,8 +36,7 @@ defmodule Iclash.DataFetcher.Supervisor do
     if start? do
       children = [
         {DynamicSupervisor, name: Iclash.DataFetcher, strategy: :one_for_one},
-        {Iclash.DataFetcher.Queue, %{rate_limit: @rate_limit, rate_limit_ms: @rate_limit_ms}},
-        {Iclash.DataFetcher.DbStatsFetcher, []}
+        {Iclash.DataFetcher.Queue, %{rate_limit: @rate_limit, rate_limit_ms: @rate_limit_ms}}
       ]
 
       Supervisor.init(children, strategy: :one_for_one)
